97ba638430342ca68c4b74d09ffb157c6c105f48
jun7867 <jjune095@naver.com>
2021-04-13 02:59:38 +0900

    Add Naver Map API , Use env

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,22 @@
 import './app.css';
+import React from 'react';
 import Navbar from './components/navbar/navbar';
+import { RenderAfterNavermapsLoaded } from 'react-naver-maps';
+import NaverMapAPI from './components/naverMap/naverMap';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <RenderAfterNavermapsLoaded
+        ncpClientId={process.env.REACT_APP_NAVERMAP_API} // 자신의 네이버 계정에서 발급받은 Client ID
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMapAPI />
+      </RenderAfterNavermapsLoaded>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/naverMap/naverMap.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NaverMap } from 'react-naver-maps';
+
+const NaverMapAPI = () => {
+  return (
+    <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      style={{
+        width: '100%', // 네이버지도 가로 길이
+        height: '85vh', // 네이버지도 세로 길이
+      }}
+      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
+      defaultZoom={13} // 지도 초기 확대 배율
+    />
+  );
+};
+
+export default NaverMapAPI;
