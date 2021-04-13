18f3a0d106024c68c25980924d7f2e4926514258
jun7867 <jjune095@naver.com>
2021-04-14 01:07:57 +0900

    Setting firebase

--- src/components/naverMap/naverMap.jsx
+++ src/components/naverMap/naverMap.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { NaverMap } from 'react-naver-maps';
+import { Marker, NaverMap } from 'react-naver-maps';
 
 const NaverMapAPI = () => {
+  const navermaps = window.naver.maps;
   return (
     <NaverMap
       mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
@@ -11,7 +12,16 @@ const NaverMapAPI = () => {
       }}
       defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
       defaultZoom={13} // 지도 초기 확대 배율
-    />
+    >
+      <Marker
+        key={1}
+        position={new navermaps.LatLng(37.551229, 126.988205)}
+        animation={2}
+        onClick={() => {
+          alert('여기는 N서울타워입니다.');
+        }}
+      />
+    </NaverMap>
   );
 };
 
